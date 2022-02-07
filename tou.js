55843428f548629e5249f027417bd0e4ea345875
Abhi Singh <thakurabhi5027@gmail.com>
2022-02-07 23:42:28 +0530

    Update tou.js

--- tou.js
+++ tou.js
@@ -1,16 +1,13 @@
-  const bu=document.querySelector('i');
-
-            window.onscroll = function(){myfun()};
-            function myfun() {
-                if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-                    bu.style.display="block";
-                }else{
-                    bu.style.display="none";
-                }
-                
-            }
-
-            bu.addEventListener("click",()=>{
-                document.body.scrollTop = 0;
-                document.documentElement.scrollTop=0;
-            })
+const bu=document.querySelector('i');
+window.onscroll = function(){myfun()};
+function myfun() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    bu.style.display="block";
+  }else{
+    bu.style.display="none";
+  }
+}
+bu.addEventListener("click",()=>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop=0;
+})
